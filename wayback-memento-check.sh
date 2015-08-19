bde19d7a5a35ef80042c7285d839e31d61467c81
Ryan Baumann <ryan.baumann@gmail.com>
2015-08-19 13:34:27 -0400

    Remove unnecessary slashes

--- wayback-memento-check.sh
+++ wayback-memento-check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-while read url; do \
+while read url; do
   echo "$url";
-  if curl -g -s --fail -I -o/dev/null "http://web.archive.org/web/${url}"; \
-    then echo "$url" >> $1; \
-    else echo "$url" >> $2; \
+  if curl -g -s --fail -I -o/dev/null "http://web.archive.org/web/${url}";
+    then echo "$url" >> $1;
+    else echo "$url" >> $2;
   fi
 done
