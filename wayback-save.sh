cf058851c298732117a43d62c480c13f4819229c
Ryan Baumann <ryan.baumann@gmail.com>
2015-08-19 13:20:29 -0400

    Add retry to save

--- wayback-save.sh
+++ wayback-save.sh
@@ -2,7 +2,7 @@
 
 while read url; do
   echo "$url";
-  if curl -g --fail -L -o/dev/null -s "http://web.archive.org/save/$url";
+  if curl -g --fail --retry 3 -L -o/dev/null -s "http://web.archive.org/save/$url";
     then echo "$url" >> $1;
     else echo "$url" >> $2;
   fi
